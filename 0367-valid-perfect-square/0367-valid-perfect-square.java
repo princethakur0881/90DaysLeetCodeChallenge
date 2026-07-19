08241d16c5268c086faaef35c13e77b60e587e1d
princethakur0881 <140616987+princethakur0881@users.noreply.github.com>
2026-07-19 20:47:53 +0530

    Time: 0 ms (100%), Space: 42 MB (57.7%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        int left = 1, right = num;
+        while (left <= right) {
+int mid = (left + right) / 2;
+// Rest of our binary search
+long midSquare = (long) mid * mid;
+
+if (midSquare == num) return true;
+
+else if (midSquare > num) right = mid - 1;
+else {
+    left = mid + 1;
+}
+        }
+return false;
+    }
+}
