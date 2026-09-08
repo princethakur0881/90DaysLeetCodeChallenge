3a22a0263f1635cf6e5276bb38feb3721e4c0174
princethakur0881 <140616987+princethakur0881@users.noreply.github.com>
2026-09-08 23:50:57 +0530

    Time: 1 ms (90.11%), Space: 43.5 MB (54.7%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.java
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.java
@@ -1,13 +1,11 @@
 class Solution {
     public int titleToNumber(String columnTitle) {
-         long ans = 0;
+        int ans=0;
 
-        // Same idea as decimal parsing, but base is 26.
-        for (int i = 0; i < columnTitle.length(); i++) {
-            char ch = columnTitle.charAt(i);
-            ans = ans * 26 + (ch - 'A' + 1);
+        for(int i=0;i<columnTitle.length();i++){
+            ans=ans*26+(columnTitle.charAt(i)-'A'+1);
         }
 
-        return (int) ans;
+        return ans;
     }
 }
