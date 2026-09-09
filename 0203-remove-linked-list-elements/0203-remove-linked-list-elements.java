e2fb68f5afe8ce20c2f4022948cb408df3131aa5
princethakur0881 <140616987+princethakur0881@users.noreply.github.com>
2026-09-09 08:11:57 +0530

    Time: 1 ms (96.93%), Space: 47.4 MB (21.57%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.java
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.java
@@ -10,14 +10,44 @@
  */
 class Solution {
     public ListNode removeElements(ListNode head, int val) {
-        if(head == null) return head;
+        // if(head == null) return head;
 
-        // while returning from stack assigning it to head.next
-        head.next = removeElements(head.next, val);
+        // // while returning from stack assigning it to head.next
+        // head.next = removeElements(head.next, val);
 
-        // if current node == val then return next value
-        if(head.val == val) return head.next;
+        // // if current node == val then return next value
+        // if(head.val == val) return head.next;
 
-        // return current node
-        return head;}
+        // // return current node
+        // return head;}
+
+        ListNode ans = new  ListNode(0,head);
+        if (head == null) {
+            return head;
+        }
+
+        ListNode prev = head;
+        ListNode curr = head;
+
+        while (curr != null) {
+            //if first node or all nodes are matching->remove head
+            if (head.val == val) {
+                head = head.next;
+                curr = head;
+                prev = head;
+            } 
+            //Not match->only update
+            else if (curr.val != val) {
+                prev = curr;
+                curr = curr.next;
+            } 
+            //value matches->delete and update
+            else {
+                prev.next = curr.next;
+                curr = curr.next;
+            }
+        }
+
+        return head;
+}
 }
